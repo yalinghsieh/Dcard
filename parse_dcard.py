aae16394e0d6b6e3ca543c6695bc465f07048d19
yalinghsieh <m10821001@yuntech.edu.tw>
2020-04-16 20:41:55 +0800

    擷取網站資料(以Dcard為例)

--- parse_dcard.py
+++ parse_dcard.py
@@ -23,27 +23,31 @@ soup = bs(html, 'html.parser')
 
 #獲得文章的看板、作者、時間
 meta_datas = []
-for x in soup.find_all('span', {'class':'sc-6oxm01-2 kkIdia'}):
+for x in soup.find_all('span', {'class': 'sc-6oxm01-2 hiTIMq'}):
     meta_datas.append(x.text.strip())
 print(meta_datas)
 
 #從meta_datas裡面挑出「看板」
 forums = []
+author = []
+times = []
 for i in range(len(meta_datas)):
     if i % 3 == 0:
         forums.append(meta_datas[i])
-    if i % 3 == 0:
+    if i % 3 == 1:
         author.append(meta_datas[i])
+    if i % 3 == 2:
+        times.append(meta_datas[i])
 
 #獲得文章標題
 titles = []
-for x in soup.find_all('h2', {'sc-1v1d5rx-3 hQqKVz'}):
+for x in soup.find_all('h2', {'class': 'sc-1v1d5rx-3 eihOFJ'}):
     titles.append(x.text)
 print(titles)
 
 #獲得文章相對連結
 hrefs = []
-for x in soup.find_all('a', {'class': 'sc-1v1d5rx-4 bMrsjN'}):
+for x in soup.find_all('a', {'class': 'sc-1v1d5rx-4 gCVegi'}):
     hrefs.append(x['href'])
 
 #從相對連結及url組成絕對連結
